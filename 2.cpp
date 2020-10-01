17416ba6c5dac312d508ea031a9db4021bb54db9
tprieto23 <tprieto@unal.edu.co>
2020-10-01 23:37:44 +0000

    ejercicio 2 final

--- 2.cpp
+++ 2.cpp
@@ -4,21 +4,24 @@ int insprime(int n);
 
 int main(void) {
   std::cout << insprime(-2) << std::endl;
-  std::cout << insprime(-2) << std::endl;
-  std::cout << insprime(-2) << std::endl;
-  std::cout << insprime(-2) << std::endl;
-  std::cout << insprime(-2) << std::endl;
-  std::cout << insprime(-2) << std::endl;
+  std::cout << insprime(0) << std::endl;
+  std::cout << insprime(2) << std::endl;
+  std::cout << insprime(3) << std::endl;
+  std::cout << insprime(4) << std::endl;
+  std::cout << insprime(41) << std::endl;
   return 0;
 }
 
 int insprime(int n){
-  int N = 1;
-  for(int ii = 2;ii < n; ++ii){
-    if(n % ii = 0){
-      N = 0;
-      ii = n;
-    }
+int N = 0;
+int c = 0;
+for(int ii = 2; ii <= n; ++ii){
+  if(n % ii == 0){
+    c++;
   }
+}
+if(c==1){
+  N = 1;
+}
 return N;
 }
